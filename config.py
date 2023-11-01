fd564bd3d662d688879ddd12cd9c4436c4bad32b
ellrobot <147643352+ellrobot@users.noreply.github.com>
2023-11-02 08:09:04 +0900

    Update config.py

--- config.py
+++ config.py
@@ -5,11 +5,11 @@ from dotenv import load_dotenv
 load_dotenv()
 
 
-API_ID = int(getenv("17250424"))
-API_HASH = getenv("753bc98074d420ef57ddf7eb1513162b")
+API_ID = int(getenv("API_ID"))
+API_HASH = getenv("API_HASH")
 
-BOT_TOKEN = getenv("6830518422:AAEPRM-cfMZjZk6yCHqg61aY3Q-qqONgot0")
-MONGO_DB_URI = getenv("mongodb+srv://1234:1234@cluster0.gb6uhwg.mongodb.net/?retryWrites=true&w=majority", None)
+BOT_TOKEN = getenv("BOT_TOKEN")
+MONGO_DB_URI = getenv("MONGO_DB_URL", None)
 
 OWNER_ID = int(getenv("OWNER_ID", 6855539355))
 SUPPORT_CHAT = getenv("SUPPORT_CHAT", "https://t.me/zyriccbase")
